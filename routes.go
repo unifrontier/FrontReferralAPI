4084be805f0dcd9012e838e0dd8f0679a7c71936
iragsraghu <raghavendra@frontier.xyz>
2022-05-04 17:51:38 +0530

    completed

--- routes.go
+++ routes.go
@@ -19,10 +19,15 @@ func ReferralData(response http.ResponseWriter, request *http.Request) {
 	var record entity.Device
 	device_id := request.FormValue("device_id")       // Get device_id from form
 	referrer_id := request.FormValue("referral_code") // Get referral_code from form
-	unique_id := referral_code.RandomString()         // 6 digit random string referral code
-	record.DeviceID = device_id                       // Serial Number of the device
-	record.UniqueID = unique_id                       // Referral Code for particular user referral
-	record.ReferrerID = referrer_id                   // Referred ID
+	if device_id == "" || referrer_id == "" {
+		response.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(response).Encode("Device id or referral code is empty")
+		return
+	}
+	unique_id := referral_code.RandomString() // 6 digit random string referral code
+	record.DeviceID = device_id               // Serial Number of the device
+	record.UniqueID = unique_id               // Referral Code for particular user referral
+	record.ReferrerID = referrer_id           // Referred ID
 
 	existing_device, err := repo.FindDevice(device_id)
 	if err != nil {
@@ -31,7 +36,7 @@ func ReferralData(response http.ResponseWriter, request *http.Request) {
 	if existing_device != nil && existing_device.DeviceID == device_id {
 		response.WriteHeader(http.StatusOK) // Send response
 		json.NewEncoder(response).Encode("Device already exists")
-		json.NewEncoder(response).Encode("Referral Code is : " + existing_device.UniqueID)
+		json.NewEncoder(response).Encode(record)
 	} else {
 		existing_referrer, err := repo.FindByReferrer(referrer_id) // Check if referrer already exists
 		if err != nil {
@@ -43,9 +48,9 @@ func ReferralData(response http.ResponseWriter, request *http.Request) {
 			repo.Update(referrer_id, device_id) // Update the device_id in the referrer table
 		}
 		repo.Save(&record)
-		response.WriteHeader(http.StatusOK)                                       // Send response
-		json.NewEncoder(response).Encode("Referral Code generated successfully")  // Send response
-		json.NewEncoder(response).Encode("Referral Code is : " + record.UniqueID) // Send response
+		response.WriteHeader(http.StatusOK)                                      // Send response
+		json.NewEncoder(response).Encode("Referral Code generated successfully") // Send response
+		json.NewEncoder(response).Encode(record)                                 // Send response
 	}
 }
 
@@ -53,6 +58,11 @@ func ReferralData(response http.ResponseWriter, request *http.Request) {
 func GetDevice(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-type", "application/json")
 	device_id := request.FormValue("device_id")
+	if device_id == "" {
+		response.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(response).Encode("Device id is required")
+		return
+	}
 	record, err := repo.FindDevice(device_id)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
@@ -85,6 +95,11 @@ func GetAllDevices(response http.ResponseWriter, request *http.Request) {
 func GetReferredCounts(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-type", "application/json")
 	device_id := request.FormValue("device_id")
+	if device_id == "" {
+		response.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(response).Encode("Device id is required")
+		return
+	}
 	record, err := repo.FindDevice(device_id)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
@@ -97,5 +112,6 @@ func GetReferredCounts(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 	response.WriteHeader(http.StatusOK) // Send response
+	json.NewEncoder(response).Encode("Referred Counts")
 	json.NewEncoder(response).Encode(len(record.ReferredIDS))
 }
