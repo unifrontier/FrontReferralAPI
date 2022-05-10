779c216b9995125905ead0d42cd1c38493fd907a
iragsraghu <raghavendra@frontier.xyz>
2022-05-10 13:29:38 +0530

    deploying to heroku

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-	const port = ":8000"
+	// const port = ":8000"
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "App Running...")
@@ -20,7 +21,7 @@ func main() {
 	router.HandleFunc("/api/v1/referral", GetDevice).Methods("GET")                // Get device by device_id
 	router.HandleFunc("/api/v1/referrals", GetAllDevices).Methods("GET")           // Get all devices
 	router.HandleFunc("/api/v1/referral/counts", GetReferredCounts).Methods("GET") // Get referral counts
-	log.Println("Server listening on port", port)
-	log.Fatalln(http.ListenAndServe(port, router))
-	// log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router)) // for production
+	// log.Println("Server listening on port", port)
+	// log.Fatalln(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router)) // for production
 }
