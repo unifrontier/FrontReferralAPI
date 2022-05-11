dcf9af42b2848d720101ba7088f6367a2dc0c0cb
iragsraghu <raghavendra@frontier.xyz>
2022-05-11 12:27:44 +0530

    commented

--- main.go
+++ main.go
@@ -4,20 +4,19 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gorilla/mux"
+	// "github.com/gorilla/mux"
 )
 
 func main() {
-	router := mux.NewRouter()
+	// router := mux.NewRouter()
 	port := os.Getenv("PORT")
 
-	router.HandleFunc("/", HomePage)
+	http.HandleFunc("/", HomePage)
 
-	router.HandleFunc("/api/v1/referral", ReferralData).Methods("POST")            // Create a new referral code
-	router.HandleFunc("/api/v1/referral", GetDevice).Methods("GET")                // Get device by device_id
-	router.HandleFunc("/api/v1/referrals", GetAllDevices).Methods("GET")           // Get all devices
-	router.HandleFunc("/api/v1/referral/counts", GetReferredCounts).Methods("GET") // Get referral counts
+	// router.HandleFunc("/api/v1/referral", ReferralData).Methods("POST")            // Create a new referral code
+	// router.HandleFunc("/api/v1/referral", GetDevice).Methods("GET")                // Get device by device_id
+	// router.HandleFunc("/api/v1/referrals", GetAllDevices).Methods("GET")           // Get all devices
+	// router.HandleFunc("/api/v1/referral/counts", GetReferredCounts).Methods("GET") // Get referral counts
 	// log.Println("Server listening on port", port)
 	// log.Fatalln(http.ListenAndServe(port, router))
 	// log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router)) // for production
